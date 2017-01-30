5b13fe690cd2e8fc13472bad33cbc45ab9b659ef
Timo Savola <timo@ninchat.com>
2017-01-30 18:22:08 +0200

    conform to PEP 8

--- pynject.py
+++ pynject.py
@@ -13,51 +13,58 @@ _clib.pynject_set.restype = ctypes.c_int
 
 _data = None
 
+
 def init():
-	if _pylib.pynject_init(b"pynject", b"_callback") != 0:
-		raise Exception("pynject init failed")
+    if _pylib.pynject_init(b"pynject", b"_callback") != 0:
+        raise Exception("pynject init failed")
+
 
 def _set(granularity):
-	assert granularity < 1
+    assert granularity < 1
 
-	if _clib.pynject_set(int(granularity * 1000000000)) != 0:
-		raise Exception("pynject set failed")
+    if _clib.pynject_set(int(granularity * 1000000000)) != 0:
+        raise Exception("pynject set failed")
+
 
 def start(granularity):
-	global _data
+    global _data
 
-	assert granularity > 0
+    assert granularity > 0
 
-	if _data is None:
-		_data = defaultdict(int)
-		_set(granularity)
-		return True
-	else:
-		return False
+    if _data is None:
+        _data = defaultdict(int)
+        _set(granularity)
+        return True
+    else:
+        return False
+
 
 def stop():
-	global _data
+    global _data
 
-	if _data is not None:
-		_set(0)
-		result = _data
-		_data = None
-		return result
-	else:
-		return None
+    if _data is not None:
+        _set(0)
+        result = _data
+        _data = None
+        return result
+    else:
+        return None
+
 
 def _callback(frame):
-	global _data
+    global _data
 
-	if _data is None:
-		return
+    if _data is None:
+        return
 
-	stack = []
+    stack = []
 
-	while frame is not None:
-		stack.append("{}({}:{})".format(frame.f_code.co_name, frame.f_globals.get("__name__"), frame.f_code.co_firstlineno))
-		frame = frame.f_back
+    while frame is not None:
+        stack.append("{}({}:{})".format(frame.f_code.co_name,
+                                        frame.f_globals.get("__name__"),
+                                        frame.f_code.co_firstlineno))
+        frame = frame.f_back
 
-	stack.reverse()
+    stack.reverse()
 
-	_data[";".join(stack)] += 1
+    _data[";".join(stack)] += 1
